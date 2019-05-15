0f88c001a17cd80d98b16083ffff4f1276aa1d78
Aviv Nachman <avivnachman@avivs-mbp.mynet>
2019-05-15 15:59:41 +0300

    changing response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req,res) => { res.send(database.users) })
+app.get('/', (req,res) => { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db)})
@@ -36,4 +36,4 @@ app.listen(PORT || 3000, () => {
     console.log(`app is running on port ${PORT}`);
 });
 
-console.log(PORT)
+
